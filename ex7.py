ab224cb330fe679181d2e34c63ae730b87375b3c
viceek <89883120+viceek@users.noreply.github.com>
2022-07-12 01:47:06 -0300

    edit

---
+++ ex7.py
@@ -0,0 +1,28 @@
+#exercicio leia um numero e mostre sua tabuada
+
+n =int(input('digite um número: '))
+
+t = n * 1
+t2 = n * 2
+t3 = n * 3
+t4 = n * 4
+t5 = n * 5
+t6 = n * 6
+t7 = n * 7
+t8 = n * 8
+t9 = n * 9
+t10 = n * 10
+
+print('a tabuada do {} está aqui' .format (n))
+print ('\n', n , 'x 1 =' , t ,)
+print ('\n', n , 'x 2 =' , t2 ,)
+print ('\n', n , 'x 3 =' , t3 ,)
+print ('\n',n , ' x 4 =' , t4 ,)
+print ('\n', n ,' x 5 =' , t5 ,)
+print ('\n', n ,' x 6 =' , t6 ,)
+print ('\n', n ,' x 7 =' , t7 ,)
+print ('\n', n ,' x 8 =' , t8 ,)
+print ('\n', n ,' x 9 =' , t9 ,)
+print ('\n', n ,' x 10 =', t10 ,)
+
+print('{} x {} = {}'. format ()) 
