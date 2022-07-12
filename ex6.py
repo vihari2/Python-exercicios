96bca88da3f228034bb9f3f7a910a796d35b7305
viceek <89883120+viceek@users.noreply.github.com>
2022-07-12 01:44:36 -0300

    m

---
+++ ex6.py
@@ -0,0 +1,13 @@
+#exercicio valor conversão de metros para cm e milimetros
+
+
+print ('=================conversão=============')
+m = int (input('Digite um número em m:')
+c = m * 100
+
+print ('o valor em cm é {}'.format (c))
+
+l = int(input('digite um núemro em c:')
+ml = l * 1000
+
+print ('o valor em milimetros é {}' .format(ml))
