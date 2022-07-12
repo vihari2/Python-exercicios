76453b91be5e03354645cd5e6b631722b9ef6357
viceek <89883120+viceek@users.noreply.github.com>
2022-07-12 01:42:01 -0300

    m

---
+++ ex4.py
@@ -0,0 +1,11 @@
+#exercicio calcular a media de um aluno
+
+a = str (input('digite o nome do aluno:'))
+
+b = int(input('digite o valor da primeira nota:'))
+
+c = int(input('digite o valor da segunda nota: '))
+
+m = (b+c)/2
+
+print('a média do aluno(a) {} é {}' .format(a,m))
