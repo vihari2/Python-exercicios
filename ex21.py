22b3916f33e82894255598ce95ba8c55dc157f2b
viceek <89883120+viceek@users.noreply.github.com>
2022-07-12 02:08:21 -0300

    feito

---
+++ ex21.py
@@ -0,0 +1,7 @@
+#excercicio Faça um programa que leia uma frase pelo teclado e mostre quantas vezes 
+#aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+frase = input('Digite um frase:  ').strip().upper()
+print('A letra A aparece em {} vezes'.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {} '.format(frase.find('A')+1))
+print('A ultima letra A apareceu na posição {}'.format(frase.rfind('A')+1))
