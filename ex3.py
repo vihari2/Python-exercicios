998437971b5ae61ca829e116a4f7103e673a7ef7
viceek <89883120+viceek@users.noreply.github.com>
2022-07-12 01:39:44 -0300

    m

---
+++ ex3.py
@@ -0,0 +1,9 @@
+n = bool(input('digite um numero:' ))
+print (n)
+
+n = input('digite algo:')
+print(n.isnumeric())
+
+a = input ('digite algo:')
+print('o tipo primitivo desse valor é',type(a))
+print('só tem espaços?', a.isspace())
